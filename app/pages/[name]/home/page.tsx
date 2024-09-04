9e91d89c169abf475abe5fbd3443176e89dc2aa8
antony-souza <antonygustavo09@hotmail.com>
2024-09-04 12:21:05 -0300

    update da tela de !token

--- app/pages/[name]/home/page.tsx
+++ app/pages/[name]/home/page.tsx
@@ -2,25 +2,27 @@
 
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
+import Image from 'next/image';
 import Sidebar from '@/app/components/doge_admin/sidebar';
 
 export default function HomePage() {
-  const [token, setToken] = useState(true); // Assume que tem token inicialmente
+  const [token, setToken] = useState<boolean>(true); // Inicialmente assume que há token
   const router = useRouter();
 
   useEffect(() => {
-    const token = localStorage.getItem('token');
-    if (!token) {
-      setToken(false); // Se não tem token, define como false(Entendimento)
+    const storedToken = localStorage.getItem('token');
+    if (!storedToken) {
+      setToken(false); // Se não há token, define como false
       setTimeout(() => {
         router.push('/pages/doge_admin');
-      }, 7000); // 7000ms = 7 segundos
+      }, 7000); // Redireciona após 7 segundos
     }
   }, [router]);
 
   if (!token) {
     return (
       <div className="flex items-center justify-center min-h-screen bg-gray-900 flex-col">
+        <Image src="/doge_token.png" width={300} height={300} alt="DogeAdmin Icon" />
         <h1 className="text-white text-6xl font-bold">Token não encontrado</h1>
         <h1 className="text-white text-4xl font-bold">Será redirecionado ao login!</h1>
       </div>
