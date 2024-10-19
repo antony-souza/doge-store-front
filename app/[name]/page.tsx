1210a4c657665b07449682c5c1481d819aa55d7d
antony-souza <antonygustavo09@hotmail.com>
2024-10-18 23:15:36 -0300

    atualizando a pagina publica

--- app/[name]/page.tsx
+++ app/[name]/page.tsx
@@ -5,6 +5,7 @@ import PublicStoreService, { IPublicPageProps } from "./services/store.public.se
 import { IStore } from "../util/interfaces-global.service";
 import { Avatar } from "@radix-ui/react-avatar";
 import { AvatarImage } from "@/components/ui/avatar";
+import { Skeleton } from "@/components/ui/skeleton"
 
 export default function PublicPage({ params }: IPublicPageProps) {
   const { name } = params;
@@ -33,17 +34,29 @@ export default function PublicPage({ params }: IPublicPageProps) {
         <section>
           {stores.length > 0 ? (
             stores.map((store) => (
-              <header key={store.id}>
+              <header
+                key={store.id}
+                style={{ backgroundColor: store.background_color || "#ffffff" }}
+                className="flex flex-col items-center p-4 rounded-b shadow-md" 
+              >
                 <Avatar>
-                  <AvatarImage src={store.image_url} alt={store.name} />
+                  <AvatarImage className="rounded-full w-32 h-32" src={store.image_url} alt={store.name} />
                 </Avatar>
+                <h1 className="text-center font-semibold">{store.name}</h1> 
               </header>
             ))
           ) : (
-            <h1>Loja não encontrada</h1>
+            <div className="flex flex-col space-y-3 justify-center items-center mt-10">
+              <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+              <div className="space-y-2">
+                <Skeleton className="h-4 w-[250px]" />
+                <Skeleton className="h-4 w-[200px]" />
+              </div>
+            </div>
           )}
         </section>
       </div>
     </>
   );
+
 }
