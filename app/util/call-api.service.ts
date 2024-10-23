933afe6558983344b36a0e907c6a8749a6c21eb2
antony-souza <antonygustavo09@hotmail.com>
2024-10-23 19:01:20 -0300

    teste

--- app/util/call-api.service.ts
+++ app/util/call-api.service.ts
@@ -6,7 +6,7 @@ export default class CallAPIService {
     private readonly baseURL: string;
 
     constructor() {
-        const apiUrlType = process.env.API_URL as API_URL || "AWS";
+        const apiUrlType = process.env.API_URL as API_URL || "LOCALHOST";
 
         this.baseURL = apiUrlType === "AWS"
             ? process.env.NEXT_PUBLIC_API_URL_LOCALHOST as string
