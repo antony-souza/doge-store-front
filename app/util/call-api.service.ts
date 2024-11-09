4d08dfdb22ebf3c9f7283ab852745b1389b3d03a
antony-souza <antonygustavo09@hotmail.com>
2024-11-09 05:07:51 -0300

    simplificando a seleção de qual api usar para o back

--- app/util/call-api.service.ts
+++ app/util/call-api.service.ts
@@ -1,19 +1,29 @@
 import { routes } from "@/router";
 
-type API_URL = "LOCALHOST" | "AWS";
+type API_URL = "LOCALHOST" | "AWS" | "NGROK";
 
 export default class CallAPIService {
     private readonly baseURL: string;
 
     constructor() {
-        const apiUrlType = process.env.API_URL as API_URL || "LOCALHOST";
+        const apiUrlType: API_URL = "NGROK";
 
-        this.baseURL = apiUrlType === "LOCALHOST"
-            ? process.env.NEXT_PUBLIC_API_URL_LOCALHOST as string
-            : process.env.NEXT_PUBLIC_API_URL_AWS as string;
+        this.baseURL = this.getBaseUrl(apiUrlType);
+
 
         if (!this.baseURL) {
             ("Base URL não definida. Verifique as variáveis de ambiente.");
+        }
+    }
+
+    private getBaseUrl(apiUrlType: API_URL): string {
+        switch (apiUrlType) {
+            case "NGROK":
+                return process.env.NEXT_PUBLIC_API_URL_NGROK as string;
+            case "AWS":
+                return process.env.NEXT_PUBLIC_API_URL_AWS as string;
+            default:
+                return process.env.NEXT_PUBLIC_API_URL_LOCALHOST as string;
         }
     }
 
